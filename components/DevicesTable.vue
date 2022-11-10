f2f7fd607cdf981c000aa4d2f988fc386552e838
deanzaka <deanzaka.h@gmail.com>
2022-11-10 11:10:06 +0700

    add color code

--- components/DevicesTable.vue
+++ components/DevicesTable.vue
@@ -1,38 +1,39 @@
 <template>
-  <v-data-table :headers="headers" :items="items" :loading="isLoading" loading-text="LOADING DATA" item-key="imei" :search="search"
-    class="elevation-1">
+  <v-data-table :headers="headers" :items="items" :loading="isLoading" loading-text="LOADING DATA" item-key="imei"
+    :search="search" class="elevation-1" :item-class="itemRowBackground">
     <template v-slot:body.prepend>
-            <tr>
-              <td></td>
-              <td></td>
-               <td>
-                <v-text-field v-model="account" type="text" label="Account"></v-text-field>
-              </td>
-              <td>
-                <v-text-field v-model="vehicleNumber" type="text" label="Vehicle Number"></v-text-field>
-              </td>
-              <td>
-                <v-text-field v-model="status" type="text" label="Status"></v-text-field>
-              </td>
-              <td>
-                <v-text-field v-model="sim" type="text" label="SIM"></v-text-field>
-              </td>
-              <td>
-                <v-text-field v-model="imei" type="text" label="IMEI"></v-text-field>
-              </td>
-              <td>
-                <v-text-field v-model="type" type="text" label="Type"></v-text-field>
-              </td>
-              <td>
-                <v-text-field v-model="remark" type="text" label="Remark"></v-text-field>
-              </td>
-            </tr>
-          </template>
+      <tr>
+        <td></td>
+        <td></td>
+        <td>
+          <v-text-field v-model="account" type="text" label="Account"></v-text-field>
+        </td>
+        <td>
+          <v-text-field v-model="vehicleNumber" type="text" label="Vehicle Number"></v-text-field>
+        </td>
+        <td>
+          <v-text-field v-model="status" type="text" label="Status"></v-text-field>
+        </td>
+        <td>
+          <v-text-field v-model="sim" type="text" label="SIM"></v-text-field>
+        </td>
+        <td>
+          <v-text-field v-model="imei" type="text" label="IMEI"></v-text-field>
+        </td>
+        <td>
+          <v-text-field v-model="type" type="text" label="Type"></v-text-field>
+        </td>
+        <td>
+          <v-text-field v-model="remark" type="text" label="Remark"></v-text-field>
+        </td>
+      </tr>
+    </template>
   </v-data-table>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
+import moment from 'moment-timezone';
 
 export default {
   computed: {
@@ -42,7 +43,7 @@ export default {
     async getDevices() {
       try {
         let account = ''
-        if(this.isUserDashboard) account = this.loginInfo.account;
+        if (this.isUserDashboard) account = this.loginInfo.account;
 
         const res = await this.$getAllDevices(account);
         if (res) {
@@ -50,6 +51,15 @@ export default {
         }
       } catch (error) {
         console.log(error)
+      }
+    },
+    itemRowBackground(item) {
+      const expiration = moment(item.expiration);
+
+      if (expiration.isBefore(moment().add(7, 'days'))) {
+        return 'red-background';
+      } else if (expiration.isBefore(moment().add(30, 'days'))) {
+        return 'yellow-background';
       }
     }
   },
@@ -60,15 +70,15 @@ export default {
   data() {
     return {
       headers: [
-        { value: 'expiration', text: 'Expiration'},
+        { value: 'expiration', text: 'Expiration' },
         { value: 'activationTime', text: 'Activation' },
-        { value: 'account', text: 'Account', filter: f => { return ( f + '' ).toLowerCase().includes( this[ 'account' ].toLowerCase() ) }  },
-        { value: 'vehicleNumber', text: 'Vehicle Number', filter: f => { return ( f + '' ).toLowerCase().includes( this[ 'vehicleNumber' ].toLowerCase() ) }  },
-        { value: 'status', text: 'Status', filter: f => { return ( f + '' ).toLowerCase().includes( this[ 'status' ].toLowerCase() ) }  },
-        { value: 'sim', text: 'SIM', filter: f => { return ( f + '' ).toLowerCase().includes( this[ 'sim' ].toLowerCase() ) }  },
-        { value: 'imei', text: 'IMEI', filter: f => { return ( f + '' ).toLowerCase().includes( this[ 'imei' ].toLowerCase() ) }  },
-        { value: 'vehicleIcon', text: 'Type', filter: f => { return ( f + '' ).toLowerCase().includes( this[ 'type' ].toLowerCase() ) }  },
-        { value: 'reMark', text: 'Remarks', filter: f => { return ( f + '' ).toLowerCase().includes( this[ 'remark' ].toLowerCase() ) }  },
+        { value: 'account', text: 'Account', filter: f => { return (f + '').toLowerCase().includes(this['account'].toLowerCase()) } },
+        { value: 'vehicleNumber', text: 'Vehicle Number', filter: f => { return (f + '').toLowerCase().includes(this['vehicleNumber'].toLowerCase()) } },
+        { value: 'status', text: 'Status', filter: f => { return (f + '').toLowerCase().includes(this['status'].toLowerCase()) } },
+        { value: 'sim', text: 'SIM', filter: f => { return (f + '').toLowerCase().includes(this['sim'].toLowerCase()) } },
+        { value: 'imei', text: 'IMEI', filter: f => { return (f + '').toLowerCase().includes(this['imei'].toLowerCase()) } },
+        { value: 'vehicleIcon', text: 'Type', filter: f => { return (f + '').toLowerCase().includes(this['type'].toLowerCase()) } },
+        { value: 'reMark', text: 'Remarks', filter: f => { return (f + '').toLowerCase().includes(this['remark'].toLowerCase()) } },
       ],
       items: [],
       isLoading: true,
@@ -84,4 +94,13 @@ export default {
   },
 }
 </script>
+
+<style>
+.red-background {
+  background-color: rgb(177, 95, 78)
+}
+.yellow-background {
+  background-color: rgba(208, 208, 96, 0.846)
+}
+</style>
 
